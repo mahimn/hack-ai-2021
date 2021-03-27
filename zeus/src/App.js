e1f3d5d76ad7408213c352c6a83ec10f1c971848
Mahim Nalluri <23005357+mahimn@users.noreply.github.com>
2021-03-27 15:06:39 -0500

    Created Base React App + Gradient

---
+++ zeus/src/App.js
@@ -0,0 +1,30 @@
+
+// Libraries
+
+// Aesthetics and Graphics
+import GradientBackground from "./components/GradientBackground/GradientBackground";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Bro
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
